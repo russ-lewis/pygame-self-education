b16bd9d54ee8493c9c5be61a2f58a098d54cf2b2
Eric <ericl16384@gmail.com>
2023-12-14 21:24:33 -0700

    used pygame.key.get_pressed() for continuous movement

--- space_invaders/space_invaders.py
+++ space_invaders/space_invaders.py
@@ -152,19 +152,26 @@ while running:
     # EVENT HANDLING
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            running = FALSE
+            running = False
 
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_ESCAPE:
                 running = False
 
-            if event.key == ord(' '):
+            # if event.key == ord(' '):
+            if event.key == pygame.K_SPACE:
                 bullets.add(gun.spawn_bullet())
 
-            if event.key == ord('a') or event.key == pygame.K_LEFT:
-                gun.rect.left -= GUN_SPEED
-            if event.key == ord('d') or event.key == pygame.K_RIGHT:
-                gun.rect.left += GUN_SPEED
+            # if event.key == ord('a') or event.key == pygame.K_LEFT:
+            #     gun.rect.left -= GUN_SPEED
+            # if event.key == ord('d') or event.key == pygame.K_RIGHT:
+            #     gun.rect.left += GUN_SPEED
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_RIGHT] or keys[pygame.K_d]:
+        gun.rect.left += GUN_SPEED
+    if keys[pygame.K_LEFT] or keys[pygame.K_a]:
+        gun.rect.left -= GUN_SPEED
+
 
 pygame.quit()
 
